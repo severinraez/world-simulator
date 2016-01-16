324cd38afecb9632b4b8e00121bf46852a0a0c1e
srz <severin.raez@bluewin.ch>
2016-01-16 16:48:51 +0100

    Added testing harness, first test

---
+++ shared/lib/object-replication.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let _ = require('underscore')
+
+let apply = (changes, target) => {
+    let keysToDelete = _.keys(changes).filter((key) => {
+	return changes[key] === undefined
+    })
+    _.each(keysToDelete, (key) => {
+	delete changes[key]
+    })
+    return _.extend(target, changes)
+}
+
+module.exports = {
+    apply: apply
+}
---
+++ test/shared/lib/object-replication.js
@@ -0,0 +1,26 @@
+'use strict';
+let expect = require('chai').expect
+let replication = require('app/shared/lib/object-replication.js')
+
+describe('object replication', () => {
+    before(function() {
+    })
+    describe('diff applying', function() {
+	before(function() {
+	    this.before = { foo: 'bar', toRemove: 'present' }
+	    this.delta = { added: 'added', toRemove: undefined, foo: 'changed' }
+	    this.after = replication.apply(this.delta, before)
+	})
+
+	it('should add keys', function() {
+	    expect(this.after).to.include.keys('added')
+	})
+	it('should remove keys', function () {
+	    expect(this.after).to.not.include.keys('toRemove')
+	})
+	it('should update keys', function() {
+	    expect(this.after).to.include.keys('foo')
+	    expect(this.after.foo).to.equal('changed')
+	})
+    })
+})
