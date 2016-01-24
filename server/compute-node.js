6b3cb866936fa98336d39437cf5f40fcd4dfda21
srz <severin.raez@bluewin.ch>
2016-01-24 21:06:08 +0100

    Document compute-node

--- server/compute-node.js
+++ server/compute-node.js
@@ -5,49 +5,68 @@ const util = require('util')
 const _ = require('underscore')
 const Promise = require('bluebird')
 
-let log = require('app/shared/lib/log.js').create('compute-node', (message) => {
+/*
+  compute-node transforms multiple processes into a master/workers cluster.
+
+  nodes are assigned roles, your responsibility is to provide a compute-node _behaviour_ for each node.
+  a compute-node definition is given as follows:
+
+  {
+    run: () => {}, // function called to set up the node. setup is considered complete when this function returns
+    source: (message) => {} // function called when the node receives a message. 
+  }
+
+  once node setup is complete, the system will consider the node *initialized*. from now on, the nodes' source may receive messages.
+*/
+
+const createLogger = require('app/shared/lib/log.js').create
+let log = createLogger('compute-node', (message) => {
     let role = thisNode.role ? thisNode.role : '(unassigned)'
     return 'node ' + role + ': ' + message
 })
 
-const MESSAGE_TYPES = {
-    init: 'worker-init',
-    ready: 'worker-ready'
+const MESSAGE_TYPES = { // non-computation internode communication
+    init: 'worker-do-init',
+    ready: 'worker-is-ready'
 }
 
-let thisNode = {
-    role: null,
-    workers: {},
-    source: null,
-    initialized: false
+let thisNode = { // state of current process' compute-node binding
+    role: null, 
+    workers: {}, // { role: [ worker (cluster.fork result), ... ]
+    sink: null, // sink for incoming data, (data) => { ... }
+    initialized: false // whether initialization is complete and the node can start to compute
 }
 
-let isMaster = () => {
-    return thisNode.role == 'master'
-}
+let isMaster = () => { return thisNode.role == 'master' }
 
-let initialize = (config, flowBuilder) => {
-    thisNode.role = cluster.isMaster ? 'master' : null
+// use the current process as a compute node
+let initialize = (config, behaviourBuilder) => {
+    if(cluster.isMaster) {
+	thisNode.role = 'master'
+    }
     if(isMaster()) {
-	return initMaster(config, flowBuilder) }
+	return initMaster(config, behaviourBuilder) }
     else {
-	return initWorker(flowBuilder) }
+	return initWorker(behaviourBuilder) }
 }
 
-let initMaster = (config, flowBuilder) => {
+// starts the workers, yields nodes' run behaviour once theyre ready
+let initMaster = (config, behaviourBuilder) => {
     thisNode.role = 'master'    
+
     log('initializing')
     return new Promise((resolve, reject) => {
-	let flow = flowBuilder('master')
-	thisNode.source = flow.source
+	let behaviour = behaviourBuilder('master')
+	thisNode.sink = behaviour.source
+
 	log('spawning workers', config)
-	let workersPromise = spawnWorkers(config, thisNode.source)
+	let workersPromise = spawnWorkers(config, thisNode.sink)
 	workersPromise.then((workers) => {
             log('workers ready')
 	    thisNode.workers = groupWorkersByRole(workers)
 	    thisNode.initialized = true	    
 
-	    resolve(flow.run)
+	    resolve(behaviour.run)
 	})
     })
 }
@@ -56,12 +75,12 @@ let initMaster = (config, flowBuilder) => {
 //@return {role: [worker, ...]}
 let groupWorkersByRole = (workers) => {
     let groupedWorkers = _.groupBy(workers, (workerData) => {
-	let [role, worker] = workerData;
+	let [role, ] = workerData;
 	return role;
     })
     _.each(_.keys(groupedWorkers), (role) => {		
 	groupedWorkers[role] = _.map(groupedWorkers[role], (workerData) => {
-	    let [role, worker] = workerData;
+	    let [, worker] = workerData;
 	    return worker;
 	})
     })
@@ -76,19 +95,31 @@ let spawnWorkers = (config, sink) => {
     return Promise.all(workerPromises)
 }
 
+/*
+  spawns a child process and coordinates it's setup as a node.
+  
+  @param role role of the node
+  @param sink source part of the compute-node definition. this function makes sure it's called with all messages the node receives
+  @return promise that resolves into [worker, ...] as soon as all worker nodes are initialized
+*/
 let spawnWorker = (role, sink) => {
     return new Promise((resolve, reject) => {
 	let worker = cluster.fork()
 	let initialized = false;
 	
 	worker.on('online', () => {
-	    // tell the worker how to initialize
+	    // tell the worker with which node definition to initialize
 	    worker.send(buildInitMessage(role));
 	})
 
 	worker.on('message', (message) => {
-	    // use first message from worker to mark him as initialized,
-	    // else delegate data to worker sink
+	    /*
+	      route internal setup messages and internode communications separately.
+
+	      the first message from the server must be one with which he reports initialization as finished.
+	      the following messages are consiedered internode communication and will be forwarded to the respective
+	      node source.
+	    */
 	    if(initialized) {
 		sink(message)
 	    }
@@ -105,39 +136,35 @@ let spawnWorker = (role, sink) => {
     })
 }
 
-let isReadyMessage = (message) => {
-    return message.type && message.type == MESSAGE_TYPES.ready
-}
+let isReadyMessage = (message) => { return message.type && message.type == MESSAGE_TYPES.ready }
+let isInitMessage = (message) => { return message.type && message.type == MESSAGE_TYPES.init }
+let buildInitMessage = (role) => { return { type: MESSAGE_TYPES.init, role: role } }
+let buildReadyMessage = () => { return { type: MESSAGE_TYPES.ready } }
 
-let isInitMessage = (message) => {
-    return message.type && message.type == MESSAGE_TYPES.init
-}
-
-let buildInitMessage = (role) => {
-    return { type: MESSAGE_TYPES.init, role: role }
-}
-
-let buildReadyMessage = () => {
-    return { type: MESSAGE_TYPES.ready }
-}
-
-let initWorker = (flowBuilder) => {
+/*
+  makes current process a worker.
+  
+  awaits configuration instructions from the master and delegates running the node to the caller.
+  
+  @return promise that will resolve to run-part of compute node definition as soon as initialization is finished.
+*/
+let initWorker = (behaviourBuilder) => {
     return new Promise((resolve, reject) => {	
 	process.on('message', (message) => {
 	    if(thisNode.initialized) {
-		thisNode.source(message)
+		thisNode.sink(message)
 	    } else {		
 		if(isInitMessage(message)) {
 		    let role = message.role
 		    thisNode.role = role
 
-		    let flow = flowBuilder(role)
-		    thisNode.source = flow.source
+		    let behaviour = behaviourBuilder(role)
+		    thisNode.sink = behaviour.source
 
 		    thisNode.initialized = true
 		    let runTap = () => {			
 			workerSend('master', buildReadyMessage())	
-			flow.run()
+			behaviour.run()
 		    }
 
 		    resolve(runTap)
--- server/index.js
+++ server/index.js
@@ -19,44 +19,10 @@ let flowPromise = computeNode.initialize(nodes, (role) => {
     if(!code) {
 	throw 'no code found for role ' + role }
 
-    log('initializing node with role ' + role + '...')
-    log('code', code)
     let flow = require(code)
-
-    log('flow', flow)
     return flow;
 })
 
 flowPromise.then((flow) => { 
-    log('got flow', flow)
     flow()
-    log('executed flow')
 })
-
-/*
-class PutCommand {
-    constructor(params) {
-	this.object = params.object
-	this.coordinates = params.coordinates
-    }
-    
-    apply (world) {
-	world.put(this.object, this.coordinates)
-    }
-}
-
-let commands = {
-    'put': PutCommand
-}
-
-network.onCommand((commandParams) => {
-    let commandKlass = commands[commandParams.name]
-    let command = new commandKlass(commandParams)
-    
-    command.apply(simulation)
-})
-
-world.onHistory((records) => {
-    network.putHistory(records)
-})
-*/
